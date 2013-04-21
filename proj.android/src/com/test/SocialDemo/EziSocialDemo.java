e991df044f4268f6509519fb07829b36a74ade9d
Paras Mendiratta <codesnooker@Parass-MacBook-Pro.local>
2013-04-21 16:07:23 +0530

    Updated Library file for using it with all package names.

--- proj.android/src/com/test/SocialDemo/EziSocialDemo.java
+++ proj.android/src/com/test/SocialDemo/EziSocialDemo.java
@@ -20,20 +20,26 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-****************************************************************************/
+ ****************************************************************************/
 package com.test.SocialDemo;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
 import android.os.Bundle;
 
-public class EziSocialDemo extends Cocos2dxActivity{
+import com.ezibyte.social.EziSocialManager;
 
-	protected void onCreate(Bundle savedInstanceState){
+public class EziSocialDemo extends Cocos2dxActivity
+{
+
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
+		EziSocialManager.setApplicationID(this.getApplicationContext().getString(R.string.app_id));
 	}
-	
-    static {
-         System.loadLibrary("game");
-    }
+
+	static
+	{
+		System.loadLibrary("game");
+	}
 }
