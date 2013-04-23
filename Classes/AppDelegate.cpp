2197a1f2d8ab4e8fa6225c26b017bd0b8a341b50
Paras Mendiratta <codesnooker@Parass-MacBook-Pro.local>
2013-04-23 14:06:04 +0530

    Updated licensing terms.

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -3,6 +3,28 @@
 #include "cocos2d.h"
 #include "HelloWorldScene.h"
 
+//
+//  AppDelegate.cpp
+//  EziScoailDemo
+//
+//  Created by Paras Mendiratta on 11/04/13.
+//  EziByte (http://www.ezibyte.com)
+//
+
+/***
+ 
+ This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+ 
+ 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+ 
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+ */
+
 USING_NS_CC;
 
 AppDelegate::AppDelegate()
@@ -21,7 +43,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
 
     // turn on display FPS
-    pDirector->setDisplayStats(true);
+    pDirector->setDisplayStats(false);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     pDirector->setAnimationInterval(1.0 / 60);
--- Classes/EziSocialObject.cpp
+++ Classes/EziSocialObject.cpp
@@ -1,10 +1,26 @@
 //
 //  EziSocialObject.cpp
-//  FacebookGameDemo
+//  EziSocialDemo
 //
 //  Created by Paras Mendiratta on 11/04/13.
 //
+//  EziByte (http://www.ezibyte.com)
 //
+
+/***
+ 
+ This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ 
+ 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+ 
+ 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+ 
+ 3. This notice may not be removed or altered from any source distribution.
+ 
+*/
+
 
 #include "EziSocialObject.h"
 #include "EziSocialDefinition.h"
